dc5f43cf17a23390975cf1a73fa300bcfe2dc63b
02Alexis <m_chael1012@hotmail.com>
2023-07-07 18:51:10 -0500

    adding new styles to the 'what we do' page components, also added new fields for sanity

--- src/components/CardWhatWeDo/CardWhatWeDo.jsx
+++ src/components/CardWhatWeDo/CardWhatWeDo.jsx
@@ -5,59 +5,58 @@ export default function CardWhatWeDo() {
   const images =
     "https://corpoayapel.org/wp-content/uploads/2020/01/galeria-3-2.jpg";
   const containerStyle = {
-    backgroundImage: `url(https://corpoayapel.org/wp-content/uploads/2020/01/galeria-5-2.jpg)`,
+    backgroundImage: `url(https://res.cloudinary.com/dgnwqr93n/image/upload/v1688153550/slide-1_fgfpri.jpg)`,
   };
+
+  const background =
+    "https://res.cloudinary.com/dgnwqr93n/image/upload/v1688153550/slide-1_fgfpri.jpg";
+
   return (
     <>
       <div className="content">
         <div className="content__one">
-          <div className="row">
-            <div className="column">
-              <img
-                src="https://corpoayapel.org/wp-content/uploads/2020/01/galeria-5-2.jpg"
-                alt="Imagen 1"
-              />
-            </div>
-            <div className="column">
-              <p>
-                Entidad sin ánimo de lucro que promueve el desarrollo sostenible
-                del complejo cenagoso de Ayapel y de su comunidad.
-              </p>
-            </div>
+          <div className="title">
+            <h2>
+              <span>Qué</span> hacemos
+            </h2>
+            <p>
+              Una entidad sin ánimo de lucro que promueve el desarrollo
+              sostenible del complejo cenagoso de Ayapel y de su comunidad.
+            </p>
+            <p>
+              Trabajamos bajo tres líneas estratégicas: social, ambiental y
+              económica.
+            </p>
+          </div>
+          <div className="contentImg">
+            <img
+              src="https://yt3.googleusercontent.com/-H4bsnS3lUHCiaDtVcHxm9dJudoCyLdjnBCaIJZSsMJPNqIJFZFqs5iaTx0OjZcxwwCxycfEnA=s900-c-k-c0x00ffffff-no-rj"
+              alt="QueHacemos"
+            />
           </div>
         </div>
 
-        <div className="content__two">
-          <div className="row">
-            <div className="column">
-              <p>
-                Entidad sin ánimo de lucro que promueve el desarrollo sostenible
-                del complejo cenagoso de Ayapel y de su comunidad.
-              </p>
-            </div>
-            <div className="column">
-              <img
-                src="https://corpoayapel.org/wp-content/uploads/2020/01/galeria-3-2.jpg"
-                alt="Imagen 2"
-              />
-            </div>
-          </div>
-        </div>
+        <div
+          className="content__img"
+          style={{
+            backgroundImage: `url(${background})`,
+          }}
+        >
+          <div className="contentAmbiental">
+            <h2>
+              Gestión <span>ambiental</span>
+            </h2>
 
-        <div className="content__three">
-          <div className="row">
-            <div className="column">
-              <img
-                src="https://res.cloudinary.com/drrpq9vlk/image/upload/v1687021110/cld-sample-5.jpg"
-                alt="Imagen 3"
-              />
-            </div>
-            <div className="column">
-              <p>
-                Entidad sin ánimo de lucro que promueve el desarrollo sostenible
-                del complejo cenagoso de Ayapel y de su comunidad.
-              </p>
-            </div>
+            <h3>Ayapel sin Basuras</h3>
+            <p>
+              Ayapel sin Basuras, es un programa sostenible y comunitario que
+              intenta solucionar uno de los principales contaminantes de la
+              ciénaga: el plástico. Queremos ser un actor principal y activo en
+              la recolección y aprovechamiento de los residuos reciclables para
+              lo cual contamos con sistema de recolección de residuos y una
+              planta de compactación de PET. Recogemos en promedio 1.000 kl de
+              material reciclable para darle su destinación adecuada.
+            </p>
           </div>
         </div>
       </div>
@@ -65,10 +64,22 @@ export default function CardWhatWeDo() {
       <div className="contentnone">
         <div>
           <div className="contentnone__img" style={containerStyle}>
-            <p>
-              Entidad sin ánimo de lucro que promueve el desarrollo sostenible
-              del complejo cenagoso de Ayapel y de su comunidad.
-            </p>
+            <div className="textAmbiental">
+              <h2>
+                Gestión <span>ambiental</span>
+              </h2>
+
+              <h3>Ayapel sin Basuras</h3>
+              <p>
+                Ayapel sin Basuras, es un programa sostenible y comunitario que
+                intenta solucionar uno de los principales contaminantes de la
+                ciénaga: el plástico. Queremos ser un actor principal y activo
+                en la recolección y aprovechamiento de los residuos reciclables
+                para lo cual contamos con sistema de recolección de residuos y
+                una planta de compactación de PET. Recogemos en promedio 1.000
+                kl de material reciclable para darle su destinación adecuada.
+              </p>
+            </div>
           </div>
         </div>
 
