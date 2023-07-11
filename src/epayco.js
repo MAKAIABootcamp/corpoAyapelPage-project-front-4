13e6a638d3332a95940e2f17a3322a04c7cf40ad
Maria Antonieta Pereira <antonietap0@gmail.com>
2023-07-10 22:39:24 -0500

    add map from googlemaps, component ayapel  and fetch to epayco

--- src/epayco.js
+++ src/epayco.js
@@ -2,12 +2,19 @@
 //import { params_transaction } from './path/to/params_transaction';
 //import { ePayco } from './path/to/ePayco';
 
-const P_CUST_ID_CLIENTE = "936779";
-const P_KEY = "54355433733cfdea3514765b57b65abac635d26c\n\n";
-const PUBLIC_KEY = "856fc378f84069d4d0ba4174647eb794";
-const PRIVATE_KEY = "863642a552fd0e5d7f8331170110be63";
+// const P_CUST_ID_CLIENTE = "936779";
+// const P_KEY = "54355433733cfdea3514765b57b65abac635d26c\n\n";
+// const PUBLIC_KEY = "856fc378f84069d4d0ba4174647eb794";
+// const PRIVATE_KEY = "863642a552fd0e5d7f8331170110be63";
+// const url_apify = "https://apify.epayco.co";
+// const token_apify = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcGlmeWVQYXljb0pXVCIsInN1YiI6OTM2Nzc5LCJpYXQiOjE2ODkwMzk4ODUsImV4cCI6MTY4OTA0MzQ4NSwicmFuZCI6IjI3ZWVmYmJkMGYyMDg1Yjc2MWI1M2VhOGZlY2IxMzI1MjQyIiwicmVzIjpmYWxzZSwiaW5hIjpmYWxzZSwiZ3VpIjo1NDIxNzYsInV1aWQiOiI1YTc4Mzk4Zi1mYzRjLTQ4NDktYmYxYy1mMjY2MGZmM2FjNTIifQ.4UuSeOWgkcghssvAxA6a-pOOGIyEh4GCNKBwonwm7Nw";
+
+const P_CUST_ID_CLIENTE = "927115";
+const P_KEY = "efbb4a968055b891305d4d18edbe0ee3e5423269\n\n";
+const PUBLIC_KEY = "8ed7aef73ad73a63416e144acfc9b9a7";
+const PRIVATE_KEY = "c853c136adce33b85843cab1ef67bec0";
 const url_apify = "https://apify.epayco.co";
-const token_apify = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcGlmeWVQYXljb0pXVCIsInN1YiI6OTM2Nzc5LCJpYXQiOjE2ODkwMzk4ODUsImV4cCI6MTY4OTA0MzQ4NSwicmFuZCI6IjI3ZWVmYmJkMGYyMDg1Yjc2MWI1M2VhOGZlY2IxMzI1MjQyIiwicmVzIjpmYWxzZSwiaW5hIjpmYWxzZSwiZ3VpIjo1NDIxNzYsInV1aWQiOiI1YTc4Mzk4Zi1mYzRjLTQ4NDktYmYxYy1mMjY2MGZmM2FjNTIifQ.4UuSeOWgkcghssvAxA6a-pOOGIyEh4GCNKBwonwm7Nw";
+const token_apify = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJhcGlmeWVQYXljb0pXVCIsInN1YiI6OTI3MTE1LCJpYXQiOjE2ODkwNDI3NDEsImV4cCI6MTY4OTA0NjM0MSwicmFuZCI6ImM0ZDY0ZmMwMWY2YTE3Y2Q1NjY5YmEyMzdmM2FmMWI5NzkwIiwicmVzIjpmYWxzZSwiaW5hIjpmYWxzZSwiZ3VpIjo1MzI3MjcsInV1aWQiOiJjOTA2NTFmMi0xZGM5LTQ0MzAtYjZlYS1lNzU0ODg1ZmVhNzEifQ.NDaSspvvxyLAZm-GEX5GhswdzxQ4MyQSJ-HH3ZUrnHA";
 
 
 // export const getDataEpayco = () => {
