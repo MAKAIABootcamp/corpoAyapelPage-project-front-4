9762944208f4d73fc1358570986c3a30a9ae02ab
02Alexis <m_chael1012@hotmail.com>
2023-07-12 13:39:30 -0500

    unique styles

--- src/components/CardWhatWeDo/CardWhatWeDo.jsx
+++ src/components/CardWhatWeDo/CardWhatWeDo.jsx
@@ -55,96 +55,30 @@ export default function CardWhatWeDo() {
         </div>
 
         {allPostData &&
-          allPostData.map((data, index) => (
-            <div
-              className="content__img"
-              key={index}
-              style={{
-                backgroundImage: `url(${data.mainImage.asset.url})`,
-              }}
-            >
-              <div className="contentAmbiental">
-                <h2>
-                  {data.content} <span>{data.content2}</span>
-                </h2>
+  allPostData.map((data, index) => (
+    <div
+    className={`content__img ${index === 1 || index === 3 ? "content__img2" : ""}`}
 
-                <h3>{data.subcontent}</h3>
-                <p>{data.textcontent}</p>
-              </div>
-            </div>
-          ))}
+      key={index}
+      style={{
+        backgroundImage: `url(${data.mainImage.asset.url})`,
+      }}
+    >
+      <div
+        className={`${
+          index === 1 ? "contentGestion" : "contentAmbiental"
+        }`}
+      >
+        <h2>
+          {data.content} <span>{data.content2}</span>
+        </h2>
 
-        {/* <div
-          className="content__img"
-          style={{
-            backgroundImage: `url(${background})`,
-          }}
-        >
-          <div className="contentAmbiental">
-            <h2>
-              Gestión <span>ambiental</span>
-            </h2>
+        <h3>{data.subcontent}</h3>
+        <p>{data.textcontent}</p>
+      </div>
+    </div>
+  ))}
 
-            <h3>Ayapel sin Basuras</h3>
-            <p>
-              Ayapel sin Basuras, es un programa sostenible y comunitario que
-              intenta solucionar uno de los principales contaminantes de la
-              ciénaga: el plástico. Queremos ser un actor principal y activo en
-              la recolección y aprovechamiento de los residuos reciclables para
-              lo cual contamos con sistema de recolección de residuos y una
-              planta de compactación de PET. Recogemos en promedio 1.000 kl de
-              material reciclable para darle su destinación adecuada.
-            </p>
-          </div>
-        </div>
-
-        <div
-          className="content__img2"
-          style={{
-            backgroundImage: `url(${background})`,
-          }}
-        >
-          <div className="contentGestion">
-            <h2>
-              Gestión <span>Social</span>
-            </h2>
-
-            <h3>Ayapel sin Basuras</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum.
-            </p>
-          </div>
-        </div>
-
-        <div
-          className="content__img"
-          style={{
-            backgroundImage: `url(${background2})`,
-          }}
-        >
-          <div className="contentAmbiental">
-            <h2>
-              Gestión <span>Economica</span>
-            </h2>
-
-            <h3>Ayapel sin Basuras</h3>
-            <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum
-            </p>
-          </div>
-        </div> */}
       </div>
 
       <div className="contentnone">
@@ -171,83 +105,6 @@ export default function CardWhatWeDo() {
               );
             })}
         </div>
-        {/* <div>
-          <div
-            className="contentnone__img"
-            style={{
-              backgroundImage: `url(${background})`,
-            }}
-          >
-            <div className="textAmbiental">
-              <h2>
-                Gestión <span>ambiental</span>
-              </h2>
-
-              <h3>Ayapel sin Basuras</h3>
-              <p>
-                Ayapel sin Basuras, es un programa sostenible y comunitario que
-                intenta solucionar uno de los principales contaminantes de la
-                ciénaga: el plástico. Queremos ser un actor principal y activo
-                en la recolección y aprovechamiento de los residuos reciclables
-                para lo cual contamos con sistema de recolección de residuos y
-                una planta de compactación de PET. Recogemos en promedio 1.000
-                kl de material reciclable para darle su destinación adecuada.
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div>
-          <div
-            className="contentnone__img"
-            style={{
-              backgroundImage: `url(${background})`,
-            }}
-          >
-            <div className="textAmbiental">
-              <h2>
-                Gestión <span>Social</span>
-              </h2>
-
-              <h3>Ayapel sin Basuras</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum
-              </p>
-            </div>
-          </div>
-        </div>
-
-        <div>
-          <div
-            className="contentnone__img"
-            style={{
-              backgroundImage: `url(${background2})`,
-            }}
-          >
-            <div className="textAmbiental">
-              <h2>
-                Gestión <span>Economica</span>
-              </h2>
-
-              <h3>Ayapel sin Basuras</h3>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum
-              </p>
-            </div>
-          </div>
-        </div> */}
       </div>
     </>
   );
