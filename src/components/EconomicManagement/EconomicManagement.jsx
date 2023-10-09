38f6ad669584a146524be5304938e1a5f1b7a06a
J-Ciro <jciro.1984@gmail.com>
2023-10-09 13:06:15 -0500

    Responsive design

--- src/components/EconomicManagement/EconomicManagement.jsx
+++ src/components/EconomicManagement/EconomicManagement.jsx
@@ -2,10 +2,11 @@ import React, { useState, useEffect } from "react";
 import client from "../../sanity/client";
 import "./EconomicManagement.scss";
 
-const EconomicManagement = ({BackGroundImage}) => {
+const EconomicManagement = ({ BackGroundImage }) => {
   const [projectData, setProjectData] = useState(null);
   const [data, setData] = useState([]);
   const [mostrarContenido, setMostrarContenido] = useState({});
+  const [isMobile, setIsMobile] = useState(false); // Variable para detectar dispositivo móvil
 
   useEffect(() => {
     client
@@ -47,59 +48,81 @@ const EconomicManagement = ({BackGroundImage}) => {
       .catch(console.error);
   }, []);
 
+  useEffect(() => {
+    // Detectar si estamos en un dispositivo móvil
+    const isMobileDevice = window.innerWidth <= 768; // Ajusta el ancho máximo según tu criterio
+    setIsMobile(isMobileDevice);
+
+    // Agregar un listener para detectar cambios en el tamaño de la ventana
+    const handleResize = () => {
+      const isMobileDevice = window.innerWidth <= 768; // Ajusta el ancho máximo según tu criterio
+      setIsMobile(isMobileDevice);
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
   const handleMouseEnter = (dataId) => {
-    setMostrarContenido({ ...mostrarContenido, [dataId]: true });
+    if (!isMobile) {
+      setMostrarContenido({ ...mostrarContenido, [dataId]: true });
+    }
   };
 
   const handleMouseLeave = (dataId) => {
-    setMostrarContenido({ ...mostrarContenido, [dataId]: false });
+    if (!isMobile) {
+      setMostrarContenido({ ...mostrarContenido, [dataId]: false });
+    }
   };
 
-  const titulo = projectData?.content;
-  const parrafoCompleto = projectData?.textcontent || "";
-  const parrafoCorto = `${parrafoCompleto.substring(0, 80)}...`;
+  const handleClick = (dataId) => {
+    if (isMobile) {
+      setMostrarContenido((prevState) => ({
+        ...prevState,
+        [dataId]: !prevState[dataId],
+      }));
+    }
+  };
 
   return (
     <>
       <div className="content" style={BackGroundImage}>
-        {projectData &&
-          projectData.mainImage && (
-            <div
-              className="content__img-Economic"
-              // style={{
-              //   backgroundImage: `url(${projectData.mainImage.asset.url})`,
-              // }}
-            >
-              <div className="content-Economic">
-                <h2>
-                  {projectData.content} <span>{projectData.content2}</span>
-                </h2>
-                <h3>{projectData.subcontent}</h3>
-                <p>{projectData.textcontent}</p>
-                <p>{projectData.textcontent2}</p>
-              </div>
-              <section className="cardsContent">
-                {data.map((item, index) => (
-                  <div key={index}>
-                    <div
-                      className={`containerProject ${
-                        mostrarContenido[index] ? "see" : ""
-                      }`}
-                      onMouseEnter={() => handleMouseEnter(index)}
-                      onMouseLeave={() => handleMouseLeave(index)}
-                    >
-                      <div className="title">{item.titleContent}</div>
-                      <div className="contentText">
-                        {mostrarContenido[index]
-                          ? item.textContent
-                          : `${item.textContent.substring(0, 50)}...`}
-                      </div>
+        {projectData && projectData.mainImage && (
+          <div className="content__img-Economic">
+            <div className="content-Economic">
+              <h2>
+                {projectData.content} <span>{projectData.content2}</span>
+              </h2>
+              <h3>{projectData.subcontent}</h3>
+              <p>{projectData.textcontent}</p>
+              {/* <p>{projectData.textcontent2}</p> */}
+            </div>
+            <section className="cardsContent">
+              {data.map((item, index) => (
+                <div key={index}>
+                  <div
+                    className={`containerProject ${
+                      mostrarContenido[index] ? "see" : ""
+                    }`}
+                    onMouseEnter={() => handleMouseEnter(index)}
+                    onMouseLeave={() => handleMouseLeave(index)}
+                    onClick={() => handleClick(index)}
+                  >
+                    <div className="title">{item.titleContent}</div>
+                    <div className="contentText">
+                      {mostrarContenido[index]
+                        ? item.textContent
+                        : `${item.textContent.substring(0, 50)}...`}
                     </div>
                   </div>
-                ))}
-              </section>
-            </div>
-          )}
+                </div>
+              ))}
+            </section>
+          </div>
+        )}
       </div>
     </>
   );
